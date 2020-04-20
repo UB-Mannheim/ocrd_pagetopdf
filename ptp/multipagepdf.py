9bd0981c3e5d7a9a851365bf5dc4639d95e3ec80
JKamlah <>
2020-04-20 14:57:25 +0200

    Rework pdfmarks function and cleansing, add merged-PDF ref to mets and making the pdf input properties more precise.

--- ptp/multipagepdf.py
+++ ptp/multipagepdf.py
@@ -7,46 +7,63 @@ import subprocess
 
 from ocrd_models import OcrdMets
 from ocrd_utils.logging import getLogger
+from atomicwrites import atomic_write
 
 def read_from_mets(metsfile, filegrp, outputfile):
     mets = OcrdMets(filename=metsfile)
     inputfiles = []
     page_ids = []
-    for f in mets.find_files(mimetype="application/pdf", fileGrp=filegrp):
-        inputfiles.append(f.local_filename)
-        page_ids.append(f.pageid)
+    for f in mets.find_files(mimetype='application/pdf', fileGrp=filegrp):
+        # ingore mulitpaged pdfs
+        if f.pageId:
+            inputfiles.append(f.local_filename)
+            page_ids.append(f.pageId)
     if inputfiles:
-        return pdfmerge(inputfiles, page_ids, outputfile)
+        if not pdfmerge(inputfiles, outputfile, pagelabels=page_ids):
+            mets.add_file(filegrp, mimetype='application/pdf', ID=outputfile, url=str(Path(filegrp).joinpath(outputfile+'.pdf')))
+            with atomic_write(metsfile, overwrite=True) as f:
+                f.write(mets.to_xml(xmllint=True).decode('utf-8'))
     return None
 
-def pdfmerge(inputfiles, page_ids, outputfile, metadata=None):
+def create_pdfmarks(pdfdir, pagelabels=None, metadata=None):
+    pdfmarks = pdfdir.joinpath('pdfmarks.ps')
+    with pdfmarks.open('w') as marks:
+        if metadata:
+            marks.write("[ ")
+            for kwarg in ['Title', 'Author', 'Keywords']:
+                if kwarg in metadata:
+                    marks.write(f"/{kwarg} ({metadata[kwarg]})\n")
+            marks.write("/DOCINFO pdfmark\n")
+        marks.write("[{Catalog} <<\n\
+                    /PageLabels <<\n\
+                    /Nums [\n")
+        for idx, pagelabel in enumerate(pagelabels):
+            #marks.write(f"1 << /S /D /St 10>>\n")
+            marks.write(f"{idx} << /P ({pagelabel}) >>\n")
+        else:
+            marks.write("] >> >> /PUT pdfmark")
+    return pdfmarks
+
+def pdfmerge(inputfiles, outputfile, pagelabels=None, metadata=None, store_tmp=False):
     log = getLogger('processor.pagetopdf')
     if isinstance(inputfiles, str):
         inputfiles = inputfiles.split(",")
     log.info("Merging PDFs..")
     try:
         pdfdir = Path(inputfiles[0]).parent
-        with open(pdfdir.joinpath("pdfmarks.ps"),"w") as marks:
-            if metadata:
-                marks.write("[ ")
-                for kwarg in ["Title","Author","Keywords"]:
-                    if kwarg in metadata:
-                        marks.write(f"/{kwarg} ({metadata[kwarg]})\n")
-                marks.write("/DOCINFO pdfmark\n")
-            marks.write("[{Catalog} <<\n\
-                        /PageLabels <<\n\
-                        /Nums [\n")
-            for idx, page_id in enumerate(page_ids):
-                #marks.write(f"1 << /S /D /St 10>>\n")
-                marks.write(f"{idx} << /P ({page_id}) >>\n")
-            marks.write("] >> >> /PUT pdfmark")
-        subprocess.Popen(f"gs -sDEVICE=pdfwrite \
+        pdfmarks = create_pdfmarks(pdfdir, pagelabels, metadata)
+        p = subprocess.Popen(f"gs -sDEVICE=pdfwrite \
                 -dNOPAUSE -dBATCH -dSAFER \
                 -sOutputFile={pdfdir.joinpath(outputfile+'.pdf')} \
                 {' '.join(inputfiles)}\
-                {pdfdir.joinpath('pdfmarks.ps')}", shell=True, stderr=subprocess.STDOUT)
+                {pdfmarks}", shell=True, stderr=subprocess.STDOUT)
+        p.communicate()
+        if not store_tmp:
+            pdfmarks.unlink()
+        return 0
     except Exception:
         log.exception(f"Couldn't merge the pdf files.")
+        return 1
 
-if __name__=="__main__":
+if __name__=='__main__':
     read_from_mets(sys.argv[1], sys.argv[2], sys.argv[3])
