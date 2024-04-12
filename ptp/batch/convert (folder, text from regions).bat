74aef332e2779708799c73de7779f34e5cf8bae5
Stefan Weil <sw@weilnetz.de>
2024-04-12 16:14:03 +0200

    Remove unused batch files
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- ptp/batch/convert (folder, text from regions).bat
+++
@@ -1,9 +0,0 @@
-
-
-::
-:: Loop through all files within the specified folder
-::
-
-FOR %%c in ("input\*.xml") DO (
-  java -jar ..\PageToPdf.jar -xml "%%c" -image "input\%%~nc.png" -pdf "output\%%~nc.pdf" -text-source R -outlines L -font "..\data\AletheiaSans.ttf"
-)
